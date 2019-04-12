c8568fd8a976bb124dae953fc39abe955c275c61
s3n0 <s3n0@users.noreply.github.com>
2019-04-12 20:24:26 +0200

    Update OscamPiconsConverter.py

--- OscamPiconsConverter.py
+++ OscamPiconsConverter.py
@@ -19,8 +19,8 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-from sys import argv as sys_argv
-from os import path as os_path
+from sys import argv as sys_argv, stdout as sys_stdout
+from os import path as os_path, system as os_system
 import glob
 import re
 
@@ -45,7 +45,7 @@ def table_from_srvid(caidsFilter = []):
                 d[sid] = caidsToAdd
     for key, values in d.iteritems():
         d[key] = list(set(values))                      # remove duplicated caids in dictionary variables (browsing through all dict.values for each one dict.key)
-    print('...done.')
+    print('...done.\n')
     return d             # { '1807': ['0D03', '0D70', '0D96', '0624'],  '00CD': ['0B00', '09AF'],  '00CA': ['1833', '1834', '1702', '1722', '09C4', '09AF'],  '00CB': ['0B00', '09AF'], ..... }
 
 def table_from_srvid2(caidsFilter = []):
@@ -57,7 +57,7 @@ def table_from_srvid2(caidsFilter = []):
     for line in data:
         if line.startswith('#') or line == '':
             continue
-        sid, caids = line.upper().split('|')[0].split(':')               # remove all chars from "|" to the end of line, and split the whole part via ":" character
+        sid, caids = line.upper().split('|')[0].split(':')               # remove the string from "|" to the end of line, and split the remaining part with the ":" character
         caids = re.sub('@[0-9a-fA-F]+', '', caids)                       # remove all strings such as "@0000A1CF" from the line
         caidsToAdd = list(set(caids.split(',')))
         if caidsFilter:    
@@ -85,7 +85,7 @@ def table_from_png_only(caidsFilter = []):
         sref   = path_to_png.split('/')[-1].split('.')[0].upper().split('_')
         sid    = sref[3].rjust(4,'0')
         d[sid] = caidsFilter
-    print('...done.')
+    print('...done.\n')
     return d
 
 def table_from_png_and_lamedb(caidsFilter = []):
@@ -115,8 +115,8 @@ def table_from_png_and_lamedb(caidsFilter = []):
 def png2tpl(sid_table):
     global CAIDS_FILTER, DIR_TPL, DIR_PNG, DIR_OSCAMCFG
     print('Converting PNG to TPL images (Base64/template format)...')
-    dir_list = glob.glob(DIR_PNG + '/*.png')
-    for path_to_png in dir_list:
+    counter = 0
+    for path_to_png in glob.glob(DIR_PNG + '/*.png'):
         sid = path_to_png.split('/')[-1].split('_')[3].rjust(4,'0').upper()
         if sid in sid_table:
             for caid in sid_table[sid]:
@@ -126,18 +126,24 @@ def png2tpl(sid_table):
                 else:
                     img.thumbnail((100,60))
                 buffer = BytesIO()
-                img.save(buffer, format='PNG')       # img.save('/tmp/temp.png', format='PNG')
+                img.save(buffer, format='PNG')           # img.save('/tmp/temp.png', format='PNG')
                 data_bin = buffer.getvalue()
                 data_string = 'data:image/png;base64,' + base64.b64encode(data_bin)
                 with open('{0}/IC_{1}_{2}.tpl'.format(DIR_TPL, caid, sid),'w') as f:
                     f.write(data_string)
-    print('...done.')
+                counter += 1
+                sys_stdout.write('> %s\r' % counter)
+                sys_stdout.flush()
+                #with open('/tmp/created_tpl_picons.log','a+') as f:
+                #    f.write('{0}/IC_{1}_{2}.tpl\n'.format(DIR_TPL, caid, sid))
+            del sid_table[sid]    
+    print('\n...done.\n')
 
 #############################################################################
 
 def table_size_checking(tbl):
     dict_length = sum( [len(x) for x in tbl.values()] )
-    print('Total picons and CAIDs found: %s' % dict_length)    
+    print('Total number of CAIDs for certain SIDs in conversion table: %s' % dict_length)
     if dict_length > 2000:
         while True:
             answer = raw_input('Warning !\nTotal number of all SrvIDs is too high !\nA lot of TPL-picons will be created !\nDo you really want to continue ?\n(y/n)\n>')
@@ -150,34 +156,39 @@ def table_size_checking(tbl):
 def show_man_page():
     print('''
 USAGE:
-    python {0} [OPTIONS] PNG_FOLDER TPL_FOLDER
+    python {0} OPTIONS SOURCE-PNG-DIR TARGET-TPL-DIR
 
 OPTIONS:
-    -i \t\t\t make TPL picons from all images, the -1 and -2 argument are ignored, but the -c argument must be specified !
-    -c CAID[,CAID,...] \t user-selected CAIDs (which are the only ones to be considered), if the argument -c is not used, all found CAIDs will be used
-    -1 \t\t\t make TPL picons by '.srvid' file
-    -2 \t\t\t make TPL picons by '.srvid2' file (created by Oscam, the file may contains a FFA channels with CAID = FFFE)
-    -o PATH \t\t oscam.srvid file directory if the script did not find the Oscam folder
-    -q \t\t\t higher quality image processing with antialiasing filter (higher quality means a bit larger file size), the default is "thumbnail" quality
+    -a \t\t\t make TPL picons from all PNG files, created just for user-selected CAIDs only
+       \t\t\t WARNING: the '-c CAIDs' argument must be specified if you use the '-a' argument!
+       \t\t\t          the '-1' and '-2' arguments will also ignored here!
+    -1 \t\t\t make TPL picons from 'oscam.srvid' file, filtered by user-selected CAIDs (if they are specified)
+    -2 \t\t\t make TPL picons from 'oscam.srvid2' file, filtered by user-selected CAIDs (if they are specified)
+       \t\t\t the 'oscam.srvid2' file can also contain FTA channels with CAID = FFFE, which could also be included as TPL picons
+    -c CAID[,CAID,...] \t user-selected CAIDs (which are the only ones to be considered)
+       \t\t\t if you do not specify the argument '-c' then all found CAIDs will be used! beware of the large number of CAIDs (TPL files)!
+    -o PATH \t\t path to 'oscam.srvid' file, if the script did not find the Oscam configuration folder automaticaly
+    -q \t\t\t higher quality image processing with antialiasing filter (higher quality means a larger file size)
+    -d \t\t\t delete the whole target TPL-directory before processing
 
 EXAMPLES:
-    python {0} -i -c 0624 /tmp/png_images /etc/tuxbox/config/oscam/piconTPL
+    python {0} -a -c 0624 -d /tmp/transparent-png-220x132 /etc/tuxbox/config/oscam/piconTPL
     python {0} -2 -c 0624,0D96,FFFE /usr/share/enigma2/picon /etc/tuxbox/config/oscam/piconTPL
     python {0} -1 -2 /tmp/png_images /etc/tuxbox/config/oscam/piconTPL
-    python {0} -o /media/hdd/oscam -1 /media/hdd/picon /media/hdd/piconTPL
+    python {0} -o /etc/tuxbox/config -q -1 /media/hdd/picon /media/hdd/piconTPL
 
 RECOMMENDED:
-    python {0} -1 -2 -c <your_caids_included_the_FFFE_caid> /usr/share/enigma2/picon /etc/tuxbox/config/oscam/piconTPL
+    python {0} -1 -2 -d -c <your_CAIDs_with_FFFE_included> /usr/share/enigma2/picon /etc/tuxbox/config/oscam/piconTPL
     '''.format(sys_argv[0])  )
 
 def prepare_arguments():    
     global CAIDS_FILTER, DIR_TPL, DIR_PNG, DIR_OSCAMCFG
     
-    if len(sys_argv) <= 2 or ('-i' in sys_argv and '-c' not in sys_argv):
+    if len(sys_argv) <= 2 or ('-a' in sys_argv and '-c' not in sys_argv):
         show_man_page()
         return False
     
-    if '-i' not in sys_argv:
+    if '-a' not in sys_argv:
         if '-o' in sys_argv:
             DIR_OSCAMCFG = sys_argv[ sys_argv.index('-o') + 1 ]
         else:
@@ -189,8 +200,10 @@ def prepare_arguments():
                 return False
         if '-1' in sys_argv and not os_path.isfile(DIR_OSCAMCFG + '/oscam.srvid'):
             print('ERROR ! The oscam.srvid file does not exist !')
+            return False
         if '-2' in sys_argv and not os_path.isfile(DIR_OSCAMCFG + '/oscam.srvid2'):
             print('ERROR ! The oscam.srvid2 file does not exist !')
+            return False
     
     if '-c' in sys_argv:
         clist = sys_argv[ sys_argv.index('-c') + 1 ].upper().split(',')
@@ -198,10 +211,11 @@ def prepare_arguments():
         print('User-selected CAIDs that will be considered: %s' % ', '.join(CAIDS_FILTER) )
     else:
         CAIDS_FILTER = []
-        print('User-selected CAIDs: <blank/empty>  (all found CAIDs will be included)')
+        print('User-selected CAIDs: <blank/empty>  (all found CAIDs will be included !)')
     
     DIR_TPL = sys_argv[-1]
     DIR_PNG = sys_argv[-2]
+    
     if not (os_path.isdir(DIR_TPL) and os_path.isdir(DIR_PNG)):
         print('ERROR ! TPL or PNG directory does not exist !')
         return False
@@ -214,17 +228,26 @@ if __name__ == "__main__" and prepare_arguments():
     
     global CAIDS_FILTER, DIR_TPL, DIR_PNG, DIR_OSCAMCFG    
     
-    if '-1' in sys_argv and '-i' not in sys_argv:
+    if '-d' in sys_argv:
+        os_system('rm -f %s' % (DIR_PNG + '/*.tpl'))
+    
+    if '-1' in sys_argv and '-a' not in sys_argv:
         table = table_from_srvid(CAIDS_FILTER)
+        #import json
+        #with open('/tmp/table-1.dat','w') as f:
+        #    f.write(json.dumps(table))
         if table_size_checking(table):
             png2tpl(table)
     
-    if '-2' in sys_argv and '-i' not in sys_argv:
+    if '-2' in sys_argv and '-a' not in sys_argv:
         table = table_from_srvid2(CAIDS_FILTER)
+        #import json
+        #with open('/tmp/table-2.dat','w') as f:
+        #    f.write(json.dumps(table))
         if table_size_checking(table):
             png2tpl(table)
     
-    if '-i' in sys_argv:
+    if '-a' in sys_argv:
         table = table_from_png_only(CAIDS_FILTER)
         if table_size_checking(table):
             png2tpl(table)
