8502d63ca7176f23f5d4723ca1005b30c56bc19c
s3n0 <s3n0@users.noreply.github.com>
2019-05-13 15:41:54 +0200

    Update softcam-feed-universal_3.0_setup_script.sh

--- softcam-feed-universal_3.0_setup_script.sh
+++ softcam-feed-universal_3.0_setup_script.sh
@@ -4,15 +4,21 @@
 ###################
 ## This script was pulled from IPK package "softcam-feed-universal_3.0_all.ipk"
 ## Improved by s3n0 / 2019-06-05
-################### 
+###################
+##
 ## This is a modified script. Beware... not an IPK-package which is useless I think, but the bash script only.
+##
 ## This bash script updates/adds a softcam feed. Just copy the script to '/tmp' folder and run it via the Shell, for example:
 ##      sh /tmp/softcam-feed-universal_3.0_setup_script.sh
+##
 ## If bash script doesn't want to execute, try to set the execution attribute:
 ##      chmod +x /tmp/softcam-feed-universal_3.0_setup_script.sh
+##
 ## To uninstall a feed source, just delete the feed source file:
 ##      rm -f /etc/opkg/secret-feed.conf
+##
 ###################
+
 
 
 ERRENIGMA="""
@@ -28,6 +34,29 @@ Python script used to determine chipset architecture not found:
 Aborted!
 ***************************************************************
 """
+
+SUCCESS="""
+***********************************************
+The new feed source was successfully installed!
+You can try to sync the list of available
+installation packages in the set top box:
+  opkg update
+***********************************************
+"""
+
+
+
+
+try_append_feed_source() {
+    if [[ `wget -S --spider "$1/Packages.gz" 2>&1 | grep '200 OK'` ]]
+    then        
+        rm -f /etc/opkg/secret-feed.conf
+        echo "src/gz secret-feed $1" > /etc/opkg/secret-feed.conf
+        echo "$SUCCESS"
+        exit 0
+    fi
+}
+
 
 
 
@@ -52,15 +81,6 @@ fi
 
 
 
-### Gets the whole string "6.2" instead of "6" cut-off string
-# DISTROVERSION=$(grep 'getImageVersion' /tmp/boxbranding.cfg | grep -oE "[^=]+$")
-
-### Gets the string "6" from the file (only an integer part without real) when the string inside the file is "6.2" (for example)
-DISTROVERSION=$(grep 'getImageVersion' /tmp/boxbranding.cfg | grep -oE "[^=]+$" | cut -c 1-1)
-
-
-
-
 ARCH=$(grep 'getImageArch' /tmp/boxbranding.cfg | grep -oE "[^=]+$")
 #if [ $ARCH == "armv7ahf-neon" ] || [ $ARCH == "cortexa15hf-neon-vfpv4" ] ; then ARCH=armv7ahf ; fi       # original line
 if [[ $ARCH == *"armv7"* ]] || [[ $ARCH == *"cortexa15"* ]] ; then ARCH=armv7ahf ; fi                     # updated line
@@ -70,25 +90,27 @@ if [[ $ARCH == *"armv7"* ]] || [[ $ARCH == *"cortexa15"* ]] ; then ARCH=armv7ahf
 
 
 
-url="http://nonpublic.space/$IMAGEBASE/$DISTROVERSION/$ARCH"
-if [[ `wget -S --spider "$url/Packages.gz" 2>&1 | grep '200 OK'` ]]
-then
-    rm -f /etc/opkg/secret-feed.conf
-    echo "src/gz secret-feed $url" > /etc/opkg/secret-feed.conf
-    echo "***********************************************"
-    echo "The new feed source was successfully installed!"
-    echo "You can try to sync the list of available"
-    echo "installation packages in the set top box:"
-    echo "  opkg update"
-    echo "***********************************************"
-else
-    echo "***********************************************"
-    echo "Failure! The feed source does not exists!"
-    echo " '$url/Packages.gz'"
-    echo "Aborted!"
-    echo "***********************************************"
-    exit 1
-fi
+### Gets the string "6" from the file (only an integer part without real) when the string inside the file is "6.2" (for example)
+DISTROVERSION=$(grep 'getImageVersion' /tmp/boxbranding.cfg | grep -oE "[^=]+$" | cut -c 1-1)
+try_append_feed_source "http://nonpublic.space/$IMAGEBASE/$DISTROVERSION/$ARCH"
+
+### Gets the whole string "6.2" instead of "6" cut-off string
+DISTROVERSION=$(grep 'getImageVersion' /tmp/boxbranding.cfg | grep -oE "[^=]+$")
+try_append_feed_source "http://nonpublic.space/$IMAGEBASE/$DISTROVERSION/$ARCH"
+
+
+
+echo "***********************************************"
+echo "Failure! The feed source does not exists!"
+echo "IMAGEBASE: $IMAGEBASE"
+echo "DISTROVERSION: $DISTROVERSION"
+echo "ARCH: $ARCH"
+echo "Script aborted !"
+echo "***********************************************"
+
+exit 1
+
+
 
 
 
@@ -104,5 +126,3 @@ fi
 ### http://nonpublic.space/oea/6/mips32el/Packages.gz
 
 
-
-exit 0
