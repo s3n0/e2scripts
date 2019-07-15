edae878e4778ba2443a197a012aa85e1a46b2d00
s3n0 <s3n0@users.noreply.github.com>
2019-07-16 01:27:13 +0200

    Create StandbyLeave.sh

---
+++ LGTV-RS232/script_called_by_Standby.py/ATV/StandbyLeave.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# if directory does not exist, must be created the new one /usr/script/
+# bash-script files must be located at this place: /usr/script/*.sh
+# new script files in your Enigma2 must to have a execution rights: $ chmod 755 /usr/script/*.sh
+
+# https://github.com/openatv/enigma2/blob/DEV/lib/python/Screens/Standby.py
+# https://www.opena.tv/english-section/32512-solution-standby-mode-lg-tv-hdmi-cec-simplink.html
+
+# shell command to Turning On the LG-TV through RS232 interface:
+
+echo "ka 01 01" > /dev/ttyS0
