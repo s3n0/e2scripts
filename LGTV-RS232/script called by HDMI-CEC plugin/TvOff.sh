3fb6d5198c9b4939b997f74663a951e9f3325ced
s3n0 <s3n0@users.noreply.github.com>
2019-07-16 01:18:56 +0200

    Delete TvOff.sh

--- LGTV-RS232/script called by HDMI-CEC plugin/TvOff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# if directory does not exist, must be created the new one /usr/script/
-# bash-script files must be located at this place: /usr/script/*.sh
-# new script files in your Enigma2 must to have a execution rights: $ chmod 755 /usr/script/*.sh
-
-# shell command to Turning Off the LG-TV through RS232 interface:
-
-echo "ka 01 00" > /dev/ttyS0
