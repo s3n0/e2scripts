68b71e6bbb466e55a2a1913bc26c4b4b0e1ecde4
s3n0 <s3n0@users.noreply.github.com>
2019-10-14 21:07:31 +0200

    Update remove_unused_languages.sh

--- remove_unused_languages.sh
+++ remove_unused_languages.sh
@@ -11,7 +11,7 @@
 opkg remove --force-depends $(ls /usr/share/enigma2/po/ | grep -v -E 'en|de|sk' | awk '{print "enigma2-locale-" $0}' | tr '\n' ' ')     # remove all except EN,DE,SK
 
 #### HOW TO REMOVE LANGUAGE PACKAGES (WITH THE HELPING OF LOOP, BY OpenATV FORUM -> https://www.opena.tv/howtos/41966-howto-sprachen-deinstallieren-nachdem-die-box-online-geflasht-wurde-post360880.html#post360880 ):
-#for R in `ls /usr/share/enigma2/po/ | grep -v -E 'de|en|sk'` ; do opkg remove --force-depends enigma2-locale-$R ; done ;                # remove all except EN,DE,SK
+#for LANG in `ls /usr/share/enigma2/po/ | grep -v -E 'de|en|sk'` ; do opkg remove --force-depends enigma2-locale-$LANG ; done ;          # remove all except EN,DE,SK
 
 #### HOW TO INSTALL LANGUAGE PACKAGES:
 #opkg install enigma2-locale-{ar,bg,ca,cs,da,el,en-gb,es,et,fa,fi,fr,fy,he,hr,hu,id,is,it,ku,lt,lv,nb,nl,no,pl,pt,pt-br,ro,ru,sk,sl,sr,sv,th,tr,uk,vi,zh-cn,zh-hk}
