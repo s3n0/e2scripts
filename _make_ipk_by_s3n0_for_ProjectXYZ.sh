908b8f72b8b2979934be7be8e7101808a3d83333
s3n0 <s3n0@users.noreply.github.com>
2019-10-28 18:22:18 +0100

    Update _make_ipk_by_s3n0_for_ProjectXYZ.sh

--- _make_ipk_by_s3n0_for_ProjectXYZ.sh
+++ _make_ipk_by_s3n0_for_ProjectXYZ.sh
@@ -1,41 +1,55 @@
 #!/bin/sh
 
-
-# sh script for Enigma2
-# the IPK package creation tool
-#
+# sh script for Enigma2 - the IPK package creation tool
 # 2018-2019, by s3n0
 #
 # Simple creation of an IPK package without the use of OPKG-TOOLS.
 # It's just a simple archiving of files or folders and then merging the created files into one.
 #
 # A note:
+#
 # The version of the plugin is taken from the "version.txt" file, which must always be present
 # in the same folder where the plugin is located. I also use it in the plugin's own algorithm.
-#
-# If the same version of the plugin has already been installed in the Enigma,
-# or if only the plugin directory was deleted without uninstalling using the OPKG package manager
-# then you must use a "force" method to install / remove plugin, such as:
-#
-#       opkg install --force-reinstall /<folder_name>/enigma2-plugin-extensions-projectxyz_1.0.181219_all.ipk
-#       opkg remove --force-remove enigma2-plugin-extensions-projectxyz
+
 
 
 
 #### specify the plugin folder, the plugin version and the IPK output file:
+
 plugin_dir=/usr/lib/enigma2/python/Plugins/Extensions/ProjectXYZ
-VER=$(cat "$plugin_dir/version.txt")
 ipk_pckgname=enigma2-plugin-extensions-projectxyz
+
+VER=$(cat "$plugin_dir/version.txt")
 ipk_filename=${ipk_pckgname}_${VER}_all.ipk
-ipk_target=/tmp/${ipk_filename}
+ipk_targetfile=/tmp/${ipk_filename}
+
+project_dir=/tmp/${RANDOM}
+build_dir=/tmp/${RANDOM}
+
+
+
+
+# ==== preinst ==== (before installing a package) ===
+# This script executes before that package will be unpacked from its Debian archive (".deb/.ipk") file. Many 'preinst' scripts stop services for packages which are being upgraded until their installation or upgrade is completed (following the successful execution of the 'postinst' script).
+#
+# ==== postinst === (after installing a package) ====
+# This script typically completes any required configuration of the package foo once foo has been unpacked from its Debian archive (".deb/.ipk") file. Often, 'postinst' scripts ask the user for input, and/or warn the user that if he accepts default values, he should remember to go back and re-configure that package as the situation warrants. Many 'postinst' scripts then execute any commands necessary to start or restart a service once a new package has been installed or upgraded.
+#
+# ==== prerm ====== (before removing a package) =====
+# This script typically stops any daemons which are associated with a package. It is executed before the removal of files associated with the package.
+#
+# ==== postrm ===== (after removing a package) ======
+# This script typically modifies links or other files associated with foo, and/or removes files created by the package. 
+
 
 
 
 #### prepare the CONTROL folder and all neccessary files into CONTROL folder:
-rm -rf $plugin_dir/CONTROL
-mkdir $plugin_dir/CONTROL
+rm -rf ${project_dir}/CONTROL
+mkdir -p ${project_dir} ${project_dir}/CONTROL
 
-cat > ${plugin_dir}/CONTROL/control << EOF
+
+cat > ${project_dir}/CONTROL/control << EOF
 Package: ${ipk_pckgname}
 Version: ${VER}
 Description: Plugin for testing purpose (Enigma2 plugin)
@@ -50,13 +64,13 @@ Depends:
 Source: N/A
 EOF
 
-cat > ${plugin_dir}/CONTROL/preinst << EOF
+cat > ${project_dir}/CONTROL/preinst << EOF
 #!/bin/sh
 rm -rf ${plugin_dir}
 exit 0
 EOF
 
-cat > ${plugin_dir}/CONTROL/postinst << EOF
+cat > ${project_dir}/CONTROL/postinst << EOF
 #!/bin/sh
 echo "*********************************************************"
 plugin_dir=${plugin_dir}
@@ -71,16 +85,20 @@ echo "*********************************************************"
 exit 0 
 EOF
 
-cat > ${plugin_dir}/CONTROL/prerm << EOF
+cat > ${project_dir}/CONTROL/prerm << EOF
 #!/bin/sh
 exit 0
 EOF
 
-cat > ${plugin_dir}/CONTROL/postrm << EOF
+cat > ${project_dir}/CONTROL/postrm << EOF
 #!/bin/sh
 rm -rf ${plugin_dir}
 exit 0
 EOF
+
+
+chmod 755 ${project_dir}/CONTROL/*
+
 
 
 
@@ -89,27 +107,34 @@ EOF
 #### WARNING - make sure your source codes are backed up !
 rm -rf $plugin_dir/*.py
 
-#### as the first, it's a neccessary to set the execution rights to all files (mainly in the '/CONTROL' directory):
-chmod -R 755 $plugin_dir
+#### translate locale language files from .PO to .MO format (Machine Object)
+#### if neccessary, install the locale translation tools:     opkg install gettext
+msgfmt --help > /dev/null 2>&1 || opkg install gettext
+for f in `find $plugin_dir/locale -name *.po`; do msgfmt -o ${f%.po}.mo $f; rm -f $f; done
 
-#### create archive file 'control.tar.gz' (without a relative path to the directory):
-cd $plugin_dir/CONTROL
-tar zcpv -f $plugin_dir/control.tar.gz *
 
-#### create archive file 'data.tar.gz' (with a relative path to the directory):
-tar zcpv -f $plugin_dir/data.tar.gz $plugin_dir/images/* $plugin_dir/plugin.pyo $plugin_dir/__init__.pyo $plugin_dir/version.txt
 
-#### create version file 'debian-binary':
-echo -e "2.0\n" > $plugin_dir/debian-binary
+#### prepare a copy of plugin_dir to project_dir
+mkdir -p ${project_dir}/${plugin_dir}
+cp -rp ${plugin_dir}/* ${project_dir}/${plugin_dir}
 
-#### combining all three files together (into IPK package):
-#### using the "ar" archiver to merge all 3 files in the following order:   1) debian-binary  2) control.tar.gz  3) data.tar.gz
-rm -f $ipk_target
-ar -r $ipk_target $plugin_dir/debian-binary $plugin_dir/control.tar.gz $plugin_dir/data.tar.gz
+#### create two archive files "control.tar.gz" and "data.tar.gz" + also the file "debian-binary" into build_dir
+mkdir -p ${build_dir}
+tar -C ${project_dir} -czf ${build_dir}/data.tar.gz . --exclude=CONTROL
+tar -C ${project_dir}/CONTROL -czf ${build_dir}/control.tar.gz .
+echo -e "2.0\n" > ${build_dir}/debian-binary
 
-#### cleaning all three temporary files + 'CONTROL' directory:
-rm -f $plugin_dir/control.tar.gz $plugin_dir/data.tar.gz $plugin_dir/debian-binary
-rm -rf $plugin_dir/CONTROL
+#### combining all three files together (into IPK package)
+#### using the "ar" archiver to merge all 3 files in the following order:   1) debian-binary   2) data.tar.gz   3) control.tar.gz
+cd ${build_dir}
+rm -f ${ipk_targetfile}
+ar -r ${ipk_targetfile} ./debian-binary ./data.tar.gz ./control.tar.gz 
+
+
+
+#### cleaning all temporary directories
+rm -rf ${project_dir} ${build_dir}
 
 
 exit
+
