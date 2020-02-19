1b84598c8796729cadc8d926870f07e5377421b0
s3n0 <s3n0@users.noreply.github.com>
2020-02-19 12:10:40 +0100

    Update oscam_emm_refresh.sh

--- oscam_emm_refresh.sh
+++ oscam_emm_refresh.sh
@@ -4,8 +4,8 @@
 ####    in the case of other Enigma, copy the script where you see fit (!!! not to the directory "/tmp" !!!)
 #### 2) assign the execution rights for script file:
 ####        chmod a+x /usr/script/oscam_emm_refresh.sh
-#### 3) to start the script between 06:00 and 23:00, every 15 minutes, add the following line into the CRON configuration file:
-####        */15 6-23 * * *     sh /usr/script/oscam_emm_refresh.sh
+#### 3) to start the script between 06:00 and 23:00, every 10 minutes, add the following line into the CRON configuration file:
+####        */10 6-23 * * *     sh /usr/script/oscam_emm_refresh.sh
 ####
 #### Note:
 ####    During editing the CRON config file (/etc/cron/crontabs/root), the CRON daemon must be stopped (as prevention),
