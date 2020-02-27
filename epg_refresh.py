eb7cd70555d52ec9c74f4885924d1185e9d17a38
s3n0 <s3n0@users.noreply.github.com>
2020-02-27 20:18:07 +0100

    Update epg_refresh.py

--- epg_refresh.py
+++ epg_refresh.py
@@ -26,7 +26,7 @@ from urllib2 import urlopen
 
 ###############################################
 
-def zapChannel(channel = '""'):               # zap channel using the Open-Webif
+def zapChannel(channel = '""'):               # zap channel using the Enigma2 Open-Webif
     if channel == ' ' or channel == '':
         channel = '""'
     response = urlopen('http://127.0.0.1/web/zap?sRef=' + channel)
@@ -46,38 +46,38 @@ def enigmaInStandby():                        # checking standby mode using the 
 
 if __name__ == "__main__" and enigmaInStandby():
     
-    bouquet_src = []
+    bouquet_SRC = []
     for fname in BOUQUET_FILES:
         with open('/etc/enigma2/' + fname, 'r') as f:
             bouquet_data = f.read().upper().split('\n')
             # remove all lines startswith '#NAME', '#SERVICE 1:64', '#DESCRIPTION'
             # and return the values from index 9 to right i.e. without the prefix '#SERVICE ' and also without the URL link (http: ..... string) if does it exist
             refs = [ ':'.join(s[9:].split(':')[0:10]) + ':' for s in bouquet_data if not (s.startswith('#NAME') or s.startswith('#SERVICE 1:64') or s.startswith('#DESCR'))  ]
-            bouquet_src += refs[:-1]       # the last entry ':' in the list variable must to be removed (created in the for-loop before)
+            bouquet_SRC += refs[:-1]       # the last entry ':' in the list variable must to be removed (created in the for-loop before)
     
     with open('/etc/enigma2/blacklist', 'r') as f:
-        blacklist_data = f.read().upper().split('\n')    
-        blacklist_src = [ ':'.join(k.split(':')[0:10]) + ':' for k in blacklist_data ][:-1]     # remove all http:// and other unwanted text from the end of lines
+        blacklist_data = f.read().upper().split('\n')
+        blacklist_SRC = [ ':'.join(k.split(':')[0:10]) + ':' for k in blacklist_data ][:-1]     # remove all http:// and other unwanted text from the end of lines
     
-    tuned_src = []
-    for src in bouquet_src:
+    tuned_SRC = []
+    for SRC in bouquet_SRC:
         # --- bouquet format for ServRefCode = service_type : 0 : service_quality : ServiceID : TransponderID : NetworkID : Namespace : 0 : 0 : 0 :
         # --- index of the ServRefCode =              0       1          2              3             4             5            6      7   8   9    ...
         # --- real example = '1:0:16:3725:C8E:3:EB0000:0:0:0:'
-        fields = src.split(':')
-        if not [ s for k in tuned_src if fields[4] +':'+ fields[5] in k ]:   # if *:TransponderID:NetworkID:* is already included in the tuned_src list, then continue to search of another value
-            if not src in blacklist_src:
-                tuned_src.append(src)
+        fields = SRC.split(':')
+        if not [ s for k in tuned_SRC if fields[4] +':'+ fields[5] in k ]:   # if *:TransponderID:NetworkID:* is already included in the tuned_SRC list, then continue to search of another value
+            if not SRC in blacklist_SRC:
+                tuned_SRC.append(SRC)
     
-    print('Number of channels/transponders to tune: %s' % len(tuned_src))
+    print('Number of channels/transponders to tune: %s' % len(tuned_SRC))
     print('Zapping the neccessary channels/transponders...')
     
-    for i, srefcode in enumerate(tuned_src, 1):
-        zapChannel(srefcode)
-        print('{:03d} / {:03d} - {}'.format(i, len(tuned_src), srefcode))
-        sleep(20)       # waiting 20 sec. for receiving and retrieving all EPG data from the stream (from the currently tuned transponder)
+    for i, SRC in enumerate(tuned_SRC, 1):
+        zapChannel(SRC)
+        print('{:03d} / {:03d} - {}'.format(i, len(tuned_SRC), SRC))
+        sleep(20)           # waiting 20 sec. for receiving and retrieving all EPG data from the stream (from the currently tuned transponder)
     
     print('...done.')
     
-    zapChannel('')
+    zapChannel('')          # shut down the tuner / stop watching DVB channel
  
