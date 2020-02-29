be0de4828d775163a493a7c308f0378acd195a2e
s3n0 <s3n0@users.noreply.github.com>
2020-02-29 15:38:46 +0100

    Update epg_download.sh

--- epg_download.sh
+++ epg_download.sh
@@ -1,40 +1,33 @@
 #!/bin/sh
 
+
 #### CRON config example - download epg.dat file, every day, at 5:00
 #### 00 5 * * *        sh /usr/script/epg_download.sh
 
-# NOTE:
-# -for sure it is recommended to set automatic and regular loading (refreshing) of epg data from the file, in the Enigma configuration
-# -OpenATV:   MENU > Settings > EPG > EPG settings > Auto-reload: "Yes"
-# -EPG refresh interval we can leave as the default value - i.e. every 24 hours
 
-# paths:
-log_file="/tmp/epg_download.log"
+online_file="http://example.com/iptv/epg.dat"           # online server with the "epg.dat" stored file
 local_file="/etc/enigma2/epg.dat"
+log_file="/tmp/epg_download.log"
 
-# online server with the "epg.dat" stored file:
-online_file="http://example.com/folder/epg.dat"
 
 if [ "$(wget -q -O - http://127.0.0.1/web/powerstate | grep '</e2instandby>' | cut -f 1)" == "false" ]; then
-    echo `date`": Enigma2 is not in Standby. EPG-file downloading script was canceled." >> $log_file
+    echo `date '+%Y-%m-%d %H:%M:%S'`": Enigma2 is not in Standby. EPG-file downloading script was canceled." >> $log_file
     exit 0
 fi
 
-if wget --spider ${online_file} 2>/dev/null     # check the existence of an online file
-then
-    init 4              # stop the Enigma2 (to leave the /etc/enigma2/epg.dat file from Enigma2 processing)
-    sleep 5s            # wait a few seconds (stopping of the Enigma2)
-    wget -q -O $local_file $online_file
-    echo `date`": $online_file file was downloaded + replaced" >> $log_file
-    init 3              # start the Enigma2 (to reload new file 'epg.dat')
-    sleep 1m            # wait 1 min. to boot the Enigma2
-    # Enigma2 will re-started into Power-On state... so, we need to switch the power state into Standby mode again !
-    # in all standard versions of OpenWebif:   0 = toggle Standby , 1 = Deep-Standby , 2 = Reboot the System , 3 = Restart the GUI (Enigma2 only)
-    # in newer versions of OpenWebif:          4 = wake-up from Standby , 5 = switch to Standby
-    wget -q -O /dev/null "http://127.0.0.1/web/powerstate?newstate=5"
-    echo `date`": Enigma2 was switched to Standby" >> $log_file
+
+if wget --spider ${online_file} 2>/dev/null; then       # check the existence of an online file
+    wget -q -O /tmp/e $online_file
+    [ -f $local_file ] || touch $local_file             # create an empty epg.dat file - if the file does not exist
+    if [ "$(stat -c%s /tmp/e)" != "$(stat -c%s $local_file)" ]; then
+        mv -f /tmp/e $local_file
+        wget -q -O - http://127.0.0.1/web/loadepg       # reload epg.dat file - using the Enigma2 Open-Webif
+        echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was downloaded, replaced and reloaded in your Enigma2" >> $log_file
+    fi
+    rm -f /tmp/e
 else
-    echo `date`": $online_file file was not found" >> $log_file
+    echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was not found !" >> $log_file
 fi
+
 
 exit 0
