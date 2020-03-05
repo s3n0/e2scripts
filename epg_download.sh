f4f249cdd17e17533d40471783eb103b346bbd36
s3n0 <s3n0@users.noreply.github.com>
2020-03-05 20:19:00 +0100

    Update epg_download.sh

--- epg_download.sh
+++ epg_download.sh
@@ -8,25 +8,24 @@
 online_file="http://example.com/iptv/epg.dat"           # online server with the "epg.dat" stored file
 local_file="/etc/enigma2/epg.dat"
 log_file="/tmp/epg_download.log"
+tmp_file="/tmp/e"
 
 
-if [ "$(wget -q -O - http://127.0.0.1/web/powerstate | grep '</e2instandby>' | cut -f 1)" == "false" ]; then
-    echo `date '+%Y-%m-%d %H:%M:%S'`": Enigma2 is not in Standby. EPG-file downloading script was canceled." >> $log_file
-    exit 0
-fi
+#if [ "$(wget -q -O - http://127.0.0.1/web/powerstate | grep '</e2instandby>' | cut -f 1)" == "false" ]; then
+#    echo `date '+%Y-%m-%d %H:%M:%S'`": Enigma2 is not in Standby. EPG-file downloading script was canceled." >> $log_file
+#    exit 0
+#fi
 
 
 if wget --spider ${online_file} 2>/dev/null; then       # check the existence of an online file
-    wget -q -O /tmp/e $online_file
-    [ -f $local_file ] || touch $local_file             # create an empty epg.dat file - if the file does not exist
-    if [ "$(stat -c%s /tmp/e)" != "$(stat -c%s $local_file)" ]; then
-        mv -f /tmp/e $local_file
-        wget -q -O - http://127.0.0.1/web/loadepg > /dev/null 2>&1        # reload epg.dat file - using the Enigma2 Open-Webif
+    if wget -q -O $tmp_file "$online_file" > /dev/null 2>&1 && [ "$(wc -c < $tmp_file)" != "$(wc -c < $local_file)" ]; then
+        mv -f $tmp_file $local_file
+        wget -q -O - "http://127.0.0.1/web/loadepg" > /dev/null 2>&1         # reload epg.dat file - using the Enigma2 Open-Webif
         echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was downloaded, replaced and reloaded in your Enigma2" >> $log_file
     else
-        echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was downloaded, not replaced (it is the same file size as local file in Enigma2)" >> $log_file
+        echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was downloaded, but not replaced (it is the same file size as local file in Enigma2)" >> $log_file
     fi
-    rm -f /tmp/e
+    rm -f $tmp_file
 else
     echo `date '+%Y-%m-%d %H:%M:%S'`": $online_file file was not found !" >> $log_file
 fi
