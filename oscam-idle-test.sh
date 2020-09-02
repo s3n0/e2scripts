f4d7b75115bd565a1375ca4864f8299f5394c6b7
s3n0 <s3n0@users.noreply.github.com>
2020-09-02 14:04:35 +0200

    Update oscam-idle-test.sh

--- oscam-idle-test.sh
+++ oscam-idle-test.sh
@@ -8,11 +8,11 @@ oscam_is_busy() {
     # TOTAL=0; for CHAN in $CHAN_REQ_LIST; do (( TOTAL += CHAN )); done
     # [ $TOTAL -gt 0 ]     # the function returns a boolean (according to the sum of all ServiceIDs watching)
 
-    # --- alternativa 1 ---
+    # --- alternative 1 ---
     #DELAY_CLIENT_LIST=$(wget -q -O - --no-check-certificate --no-cache --no-cookies "http://${USR}:${PAS}@127.0.0.1:8888/oscamapi.html?part=status" | sed -rn '/type="c"/,/client>/ {s/.*idle="([0-9]*)".*/\1/p}')
-    # --- alternativa 2 ---
+    # --- alternative 2 ---
     #DELAY_CLIENT_LIST=$(wget -q -O - --no-check-certificate --no-cache --no-cookies "http://${USR}:${PAS}@127.0.0.1:8888/oscamapi.html?part=status" | awk '/type="c"/,/client>/' | sed -rn 's/.*idle="([0-9]*)".*/\1/p')
-    # --- alternativa 3 ---
+    # --- alternative 3 ---
     DELAY_CLIENT_LIST=$(wget -q -O - --no-check-certificate --no-cache --no-cookies "http://${USR}:${PAS}@127.0.0.1:8888/oscamapi.html?part=status" | sed -rn '/type="c"/,/client>/ {s/.*idle="([0-9]*)".*/\1/p}')
     
     for DELAY in $DELAY_CLIENT_LIST; do [ $DELAY -lt 20 ] && break; done     # if at least one client accesses Oscam within 20 seconds, the function returns the value "0", otherwise the function returns "1"
