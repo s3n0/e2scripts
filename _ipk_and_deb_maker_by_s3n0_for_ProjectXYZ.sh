19039943a8d48cfe47a02ac7150afa11fb46184f
s3n0 <s3n0@users.noreply.github.com>
2021-01-31 13:53:27 +0100

    Update _ipk_and_deb_maker_by_s3n0_for_ProjectXYZ.sh

--- _ipk_and_deb_maker_by_s3n0_for_ProjectXYZ.sh
+++ _ipk_and_deb_maker_by_s3n0_for_ProjectXYZ.sh
@@ -14,45 +14,51 @@
 
 
 
-#### specify the plugin folder, the plugin version and the IPK output file:
+#### specify the plugin folder, the plugin version, the package output path + package filename, etc. :
 
-plugin_dir=/usr/lib/enigma2/python/Plugins/Extensions/ProjectXYZ
-ipk_pckgname=enigma2-plugin-extensions-projectxyz
+PLUGIN_NAME="ProjectXYZ"
 
-VER=$(cat "$plugin_dir/version.txt")                # VER="1.0.210130"
+PLUGIN_DIR="/usr/lib/enigma2/python/Plugins/Extensions/${PLUGIN_NAME}"
+PLUGIN_LANG_DIR="${PLUGIN_DIR}/locale"
+PROJECT_DIR="/tmp/${RANDOM}"
+BUILD_DIR="/tmp/${RANDOM}"
 
-ipk_filename=${ipk_pckgname}_${VER}_all.ipk
-ipk_targetfile=/tmp/${ipk_filename}
+VER=$(cat "$PLUGIN_DIR/version.txt")        # VER="1.0.210130"
 
-project_dir=/tmp/${RANDOM}
-build_dir=/tmp/${RANDOM}
-
-
+IPK_PCKGNAME="enigma2-plugin-extensions-${PLUGIN_NAME,,}"
+IPK_FILENAME="${IPK_PCKGNAME}_${VER}_all.ipk"
+IPK_FINISHED="/tmp/${IPK_FILENAME}"
 
 
-# ==== preinst ==== (before installing a package) ===
-# This script executes before that package will be unpacked from its Debian archive (".deb/.ipk") file. Many 'preinst' scripts stop services for packages which are being upgraded until their installation or upgrade is completed (following the successful execution of the 'postinst' script).
+
+
+# ==== preinst ========= (before installing a package) =====
+# This script executes before that package will be unpacked from its Debian archive (".deb/.ipk") file. 
+# Many 'preinst' scripts stop services for packages which are being upgraded until their installation or
+# upgrade is completed (following the successful execution of the 'postinst' script).
 #
-# ==== postinst === (after installing a package) ====
-# This script typically completes any required configuration of the package foo once foo has been unpacked from its Debian archive (".deb/.ipk") file. Often, 'postinst' scripts ask the user for input, and/or warn the user that if he accepts default values, he should remember to go back and re-configure that package as the situation warrants. Many 'postinst' scripts then execute any commands necessary to start or restart a service once a new package has been installed or upgraded.
+# ==== postinst ======== (after installing a package) ======
+# This script typically completes any required configuration of the package foo once foo has been unpacked from its Debian archive (".deb/.ipk") file.
+# Often, 'postinst' scripts ask the user for input, and/or warn the user that if he accepts default values, he should remember to go back and re-configure that package
+# as the situation warrants. Many 'postinst' scripts then execute any commands necessary to start or restart a service once a new package has been installed or upgraded.
 #
-# ==== prerm ====== (before removing a package) =====
+# ==== prerm =========== (before removing a package) =======
 # This script typically stops any daemons which are associated with a package. It is executed before the removal of files associated with the package.
 #
-# ==== postrm ===== (after removing a package) ======
-# This script typically modifies links or other files associated with foo, and/or removes files created by the package. 
+# ==== postrm ========== (after removing a package) ========
+# This script typically modifies links or other files associated with foo, and/or removes files created by the package.
 
 
 
 
-#### prepare the CONTROL folder and all neccessary files into CONTROL folder:
-rm -rf ${project_dir}/CONTROL
-mkdir -p ${project_dir} ${project_dir}/CONTROL
+#### prepare the CONTROL folder and all neccessary shell scripts inside of this folder:
+rm -rf ${PROJECT_DIR}
+mkdir -p ${PROJECT_DIR} ${PROJECT_DIR}/CONTROL
 
 
 
-cat > ${project_dir}/CONTROL/control << EOF
-Package: ${ipk_pckgname}
+cat > ${PROJECT_DIR}/CONTROL/control << EOF
+Package: ${IPK_PCKGNAME}
 Version: ${VER}
 Description: Plugin for testing purpose (Enigma2 plugin)
 Section: extra
@@ -60,16 +66,16 @@ Priority: optional
 Maintainer: s3n0
 License: GPLv3
 Architecture: all
-OE: ${ipk_pckgname}
+OE: ${IPK_PCKGNAME}
 Homepage: N/A
 Depends:
 Source: N/A
 EOF
 
-cat > ${project_dir}/CONTROL/postinst << EOF
+cat > ${PROJECT_DIR}/CONTROL/postinst << EOF
 #!/bin/sh
 echo "*********************************************************"
-echo "                  ProjectXYZ ${VER}                  "
+echo "                  ${PLUGIN_NAME} ${VER}                  "
 echo "                Enigma2 plugin/extensions                "
 echo "                   by s3n0 , 2018-2021                   "
 echo "*********************************************************"
@@ -78,12 +84,12 @@ echo "*********************************************************"
 exit 0 
 EOF
 
-cat > ${project_dir}/CONTROL/postrm << EOF
+cat > ${PROJECT_DIR}/CONTROL/postrm << EOF
 #!/bin/sh
 [ "$1" != "upgrade" ] || exit 0 > /dev/null 2>&1               # prevent the OE2.5+ based Enigma2 for deleting files when the package is "upgrading"
-rm -rf ${plugin_dir}
+rm -rf ${PLUGIN_DIR}
 echo "*********************************************************"
-echo "                  ProjectXYZ ${VER}                  "
+echo "                  ${PLUGIN_NAME} ${VER}                  "
 echo "                Enigma2 plugin/extensions                "
 echo "                   by s3n0 , 2018-2021                   "
 echo "*********************************************************"
@@ -93,59 +99,66 @@ exit 0
 EOF
 
 
-chmod a+x ${project_dir}/CONTROL/*
+chmod a+x ${PROJECT_DIR}/CONTROL/*
 
 
 
 
-#### translate locale language files from .PO to .MO format (Machine Object)
-#### if neccessary, install the locale translation tools:     opkg install gettext
-msgfmt --help > /dev/null 2>&1 || opkg install gettext
-for f in `find $plugin_dir/locale -name *.po`; do msgfmt -o ${f%.po}.mo $f; rm -f $f; done
+
+#### translate language files from .PO to .MO format (Machine Object)
+#### if necessary, install the language translation tools:  opkg install gettext
+msgfmt --help > /dev/null 2>&1 || opkg install gettext          # if the translation tool fails to start, an attempt is made to install it
+msgfmt --help > /dev/null 2>&1 \
+ && { for f in `find $PLUGIN_LANG_DIR -name *.po`; do msgfmt -o ${f%.po}.mo $f; rm -f $f; done; } \
+ || echo -e "--- Warning ! \n--- The 'msgfmt' tool or the 'gettext' package does not exist. \n--- Unable to translate language '.po' files to '.mo' format."
 
 
 
-#### prepare a copy of $plugin_dir to $project_dir
-mkdir -p ${project_dir}/${plugin_dir}
-cp -rp ${plugin_dir}/* ${project_dir}/${plugin_dir}
+
+#### prepare a copy of $PLUGIN_DIR to $PROJECT_DIR
+mkdir -p ${PROJECT_DIR}/${PLUGIN_DIR}
+cp -rp ${PLUGIN_DIR}/* ${PROJECT_DIR}/${PLUGIN_DIR}
 
 #### remove all unnecessary files and folders:
-rm -rf ${project_dir}/${plugin_dir}/*.py            # remove all source-code python files
-#rm -rf ${project_dir}/${plugin_dir}/*.pyo           # remove all compiled python files
-#rm -rf ${project_dir}/${plugin_dir}/__pycache__
+rm -rf ${PROJECT_DIR}/${PLUGIN_DIR}/*.py            # remove all source-code python files
+#rm -rf ${PROJECT_DIR}/${PLUGIN_DIR}/*.pyo           # remove all compiled python files
+#rm -rf ${PROJECT_DIR}/${PLUGIN_DIR}/__pycache__
 
-#### create archive files "control.tar.gz" + "data.tar.gz" + also create the file "debian-binary" in $build_dir
-mkdir -p ${build_dir}
-tar -C ${project_dir} --exclude='CONTROL' -czf ${build_dir}/data.tar.gz .
-tar -C ${project_dir}/CONTROL -czf ${build_dir}/control.tar.gz .
-echo -e "2.0" > ${build_dir}/debian-binary
+#### create archive files "control.tar.gz" + "data.tar.gz" + also create the file "debian-binary" in $BUILD_DIR
+mkdir -p ${BUILD_DIR}
+tar -C ${PROJECT_DIR} --exclude='CONTROL' -czf ${BUILD_DIR}/data.tar.gz .
+tar -C ${PROJECT_DIR}/CONTROL -czf ${BUILD_DIR}/control.tar.gz .
+echo -e "2.0" > ${BUILD_DIR}/debian-binary
 
 #### combining all three files together (into IPK package)
 #### using the "ar" archiver to merge all 3 files in the following order:   1) debian-binary    2) control.tar.gz    3) data.tar.gz
-cd ${build_dir}
-rm -f ${ipk_targetfile}
-ar -r ${ipk_targetfile} ./debian-binary ./control.tar.gz ./data.tar.gz
+cd ${BUILD_DIR}
+rm -f ${IPK_FINISHED}
+ar -r ${IPK_FINISHED} ./debian-binary ./control.tar.gz ./data.tar.gz
+
 
 
 
 #### make a copy from .ipk package to .deb package
 #### replace "/" by " " character from the end of filenames inside the "ar" archive
-cp -f ${ipk_targetfile} ${ipk_targetfile%.ipk}.deb
-sed -i 's/debian-binary\//debian-binary /g' ${ipk_targetfile%.ipk}.deb
-sed -i 's/control.tar.gz\//control.tar.gz /g' ${ipk_targetfile%.ipk}.deb
-sed -i 's/data.tar.gz\//data.tar.gz /g' ${ipk_targetfile%.ipk}.deb
+cp -f ${IPK_FINISHED} ${IPK_FINISHED%.ipk}.deb
+sed -i 's/debian-binary\//debian-binary /g' ${IPK_FINISHED%.ipk}.deb
+sed -i 's/control.tar.gz\//control.tar.gz /g' ${IPK_FINISHED%.ipk}.deb
+sed -i 's/data.tar.gz\//data.tar.gz /g' ${IPK_FINISHED%.ipk}.deb
 
 #replaceByte() {
 #    printf "$(printf '\\x%02X' $3)" | dd of="$1" bs=1 seek=$2 count=1 conv=notrunc &> /dev/null
 #} #### USAGE:  replaceByte "filename" offset byte
-#replaceByte "${ipk_targetfile%.ipk}.deb" 21 32    
-#replaceByte "${ipk_targetfile%.ipk}.deb" 86 32
-#sed -i 's/data.tar.gz\//data.tar.gz /g' ${ipk_targetfile%.ipk}.deb
+#replaceByte "${IPK_FINISHED%.ipk}.deb" 21 32    
+#replaceByte "${IPK_FINISHED%.ipk}.deb" 86 32
+#sed -i 's/data.tar.gz\//data.tar.gz /g' ${IPK_FINISHED%.ipk}.deb
+
 
 
 
 #### remove all temporary directories
-rm -rf ${project_dir} ${build_dir}
+rm -rf ${PROJECT_DIR} ${BUILD_DIR}
+
 
 
 
