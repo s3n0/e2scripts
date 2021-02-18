f2981f2dfb2568f7c64d5f8b089b12d2c4793bc3
s3n0 <s3n0@users.noreply.github.com>
2021-02-18 01:59:57 +0100

    Update vhannibal-setting-download.sh

--- vhannibal-setting-download.sh
+++ vhannibal-setting-download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Shell script was created by s3n0, 2021-02
+
+# Shell script was created by s3n0, 02.2021
 # - shell script was intended for downloading the patricular zip-package / "setting" package
 # - after download the zip-package, it is unpacked in a temporary folder
 # - next, at least two strings are searched in all "*.tv" files to identify a specific one file - specific SAT provider
@@ -11,38 +12,44 @@
 
 HR="#####################################################"
 echo "$HR"
-TMP_DIR="/tmp/vhannibal-temp"
+TMP_DIR="/tmp/vhannibal_settings"
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 
 
-echo "Downloading and extracting the Vhannibal setting archive file."
-wget -O "${TMP_DIR}/setting.zip" --no-check-certificate "https://www.vhannibal.net/download_setting.php?id=13&action=download"
-# NOTE: find your own setting zip-package on the Vhannibal-Enigma2 website https://www.vhannibal.net/enigma2.php
-#       and edit the required "id=??" in the URL, according to your setting zip/setting-package
-unzip -j "${TMP_DIR}/setting.zip" -d $TMP_DIR
+#### Notice:   Find your own setting zip-package on the Vhannibal web-site https://www.vhannibal.net/enigma2.php
+####            and edit the required "id=??" number, according to your downloaded zip-file.
+ID_NUMBER=13
+echo "- downloading and extracting the main Vhannibal enigma2 settings file, with ID number ${ID_NUMBER}"
+wget -O "${TMP_DIR}/setting.zip" --no-check-certificate "https://www.vhannibal.net/download_setting.php?id=${ID_NUMBER}&action=download" > /dev/null 2>&1
+[ "$?" -ne "0" ] && { echo "! ERROR ! The main Vhannibal enigma2 settings file, with ID number ${ID_NUMBER} - download failed !"; exit 1; }
+unzip -qj "${TMP_DIR}/setting.zip" -d $TMP_DIR
 
 
-echo "Search for 'Skylink' userbouquet file - with 'Movies' category."
-FILE1=$(grep -l "Movies" `grep -l "Skylink" $TMP_DIR/*.tv`)
-if [ -z "$FILE1" ]; then
-    echo "SKYLINK userbouquet file was NOT found !"
+BOUQUET2SAVE=/etc/enigma2/userbouquet.sat-skylink-vhannibal.tv
+echo "- search for 'Skylink' userbouquet file (with 'Movies' category)"
+BOUQUET2FIND=$(grep -l "Movies" `grep -l "Skylink" $TMP_DIR/*.tv`)
+if [ -z "$BOUQUET2FIND" ]; then
+    echo "! 'Skylink' userbouquet file was NOT found !"
 else
-    echo "SKYLINK userbouquet file found: ${FILE1}"
-    sed -i -e 's/NAME Skylink/NAME SAT Skylink SK \& CZ (Vhannibal '$(date "+%Y-%m")')/I' $FILE1
-    cp -f $FILE1 /etc/enigma2/userbouquet.sat-skylink-vhannibal.tv
+    echo "- 'Skylink' userbouquet file found: ${BOUQUET2FIND}"
+    sed -i -e 's/NAME Skylink/NAME SAT Skylink SK \& CZ (Vhannibal '$(date "+%Y-%m")')/I' $BOUQUET2FIND
+    echo "- copying: ${BOUQUET2FIND} ----> ${BOUQUET2SAVE}"
+    cp -f $BOUQUET2FIND $BOUQUET2SAVE
 fi
 
 
-echo "Search for 'Deutschland' userbouquet file - with 'Regionalen' category."
-FILE2=$(grep -l "Regionalen" `grep -l "Deutschland" $TMP_DIR/*.tv`)
-if [ -z "$FILE2" ]; then
-    echo "DEUTSCHLAND userbouquet file was NOT found !"
+BOUQUET2SAVE=/etc/enigma2/userbouquet.sat-deutschland-vhannibal.tv
+echo "- search for 'Deutschland' userbouquet file (with 'Regionalen' category)"
+BOUQUET2FIND=$(grep -l "Regionalen" `grep -l "Deutschland" $TMP_DIR/*.tv`)
+if [ -z "$BOUQUET2FIND" ]; then
+    echo "! 'Deutschland' userbouquet file was NOT found !"
 else
-    echo "DEUTSCHLAND userbouquet file found: ${FILE2}"
-    sed -i -e 's/NAME HD+ Deutschland/NAME SAT Deutschland (Vhannibal '$(date "+%Y-%m")')/I' $FILE2
-    cp -f $FILE2 /etc/enigma2/userbouquet.sat-deutschland-vhannibal.tv
+    echo "- 'Deutschland' userbouquet file found: ${BOUQUET2FIND}"
+    sed -i -e 's/NAME HD+ Deutschland/NAME SAT Deutschland (Vhannibal '$(date "+%Y-%m")')/I' $BOUQUET2FIND
+    echo "- copying: ${BOUQUET2FIND} ----> ${BOUQUET2SAVE}"
+    cp -f $BOUQUET2FIND $BOUQUET2SAVE
 fi
 
 
@@ -53,4 +60,6 @@ wget -qO- "http://127.0.0.1/web/servicelistreload?mode=4" > /dev/null 2>&1
 
 rm -rf $TMP_DIR
 echo "$HR"
+
+
 exit 0
