49c720b83499641b1e30911545ce357d84053c7f
s3n0 <s3n0@users.noreply.github.com>
2021-03-02 18:37:00 +0100

    Update oscam-srvid-generator-satelitnatv.sh

--- oscam-srvid-generator-satelitnatv.sh
+++ oscam-srvid-generator-satelitnatv.sh
@@ -89,7 +89,7 @@ create_srvid_file "https://www.satelitnatv.sk/freesat-by-upc-direct/" "FreeSAT" 
 
 
 ### backup the original file "oscam.srvid" to the "/tmp" dir + merge all generated ".srvid" files into one file + move this new file to the Oscam config-dir:
-[ -n "$OSCAM_CFGDIR" ] && mv "${OSCAM_CFGDIR}/oscam.srvid" "/tmp/oscam_-_backup_$(date '+%y-%m-%d_%H-%M-%S').srvid"         # backup the older 'oscam.srvid' file
+[ -n "$OSCAM_CFGDIR" ] && mv "${OSCAM_CFGDIR}/oscam.srvid" "/tmp/oscam_-_backup_$(date '+%Y-%m-%d_%H-%M-%S').srvid"         # backup the older 'oscam.srvid' file
 echo "$HEADER" > $OSCAM_SRVID
 cat /tmp/oscam__* >> $OSCAM_SRVID
 rm -f /tmp/oscam__*
