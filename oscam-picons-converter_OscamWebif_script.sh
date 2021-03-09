066979ba331d23adc19ed46211a7a747782a3099
s3n0 <s3n0@users.noreply.github.com>
2021-03-09 11:50:52 +0100

    Add files via upload

---
+++ oscam-picons-converter_OscamWebif_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PYSCRIPT="/usr/script/oscam-picons-converter.py"
+PARAMS="-q -1 -c 0624,0D96"
+
+/usr/bin/python $PYSCRIPT $PARAMS > /tmp/oscam-picons-converter.log 2>&1 &
+
+echo "The python script 'oscam-picons-converter.py' has been started ... it will run in the background."
+
+exit 0
