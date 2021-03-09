1dab9c230d9703be40b7b985ba60f1200fc08a31
s3n0 <s3n0@users.noreply.github.com>
2021-03-09 11:59:02 +0100

    Update oscam-picons-converter_OscamWebif_script.sh

--- oscam-picons-converter_OscamWebif_script.sh
+++ oscam-picons-converter_OscamWebif_script.sh
@@ -5,6 +5,9 @@ PARAMS="-d -q -1 -c 0624,0D96"
 
 /usr/bin/python $PYSCRIPT $PARAMS > /tmp/oscam-picons-converter.log 2>&1 &
 
-echo "The python script 'oscam-picons-converter.py' has been started ... it will run in the background."
+echo "The python script 'oscam-picons-converter.py' has been started - it will run in the background."
+echo "To verify that the script is working properly, you can check the '/tmp/oscam-picons-converter.log' file,"
+echo "after a few minutes or seconds (depending on the pikon generation speed)."
+echo "Bye."
 
 exit 0
