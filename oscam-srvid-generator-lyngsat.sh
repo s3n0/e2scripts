08fd700b8be5ff8174d1699ba37cc34dc54b52b2
s3n0 <s3n0@users.noreply.github.com>
2021-03-15 22:35:16 +0100

    Update oscam-srvid-generator-lyngsat.sh

--- oscam-srvid-generator-lyngsat.sh
+++ oscam-srvid-generator-lyngsat.sh
@@ -60,8 +60,13 @@ create_srvid_file()
     SID_MATCH='<td align="center" bgcolor="#[0-9a-f]+"><font face="Verdana" size=[0-9]+>[0-9 ]+</td>'
     
     LIST=$(cat /tmp/los.html | grep -E -e "${CHN_MATCH}" -e "${SID_MATCH}")
-    
     rm -f /tmp/los.html
+    
+    LIST_LEN=$(printf "%s" "$LIST" | grep -c "^")
+    if (( $LIST_LEN % 2 )); then            # testing whether the number of lines in the $LIST variable is even (must be even)
+        echo -e "ERROR !\nThe SID + CAID list from the web page is not complete (number of lines is odd, but an even number is required).\nThe script will be aborted !"
+        exit 1
+    fi
     
     # Example of the $LIST variable content:
     #
@@ -71,22 +76,22 @@ create_srvid_file()
     # <td bgcolor="#ffcc99"><font face="Arial"><font size=2><b><a href="https://www.lyngsat.com/tvchannels/us/ESPN-2-Africa.html">ESPN 2 Africa</a></b></td>
     # <td align="center" bgcolor="#ffcc99"><font face="Verdana" size=1> 2420</td>
     # <td bgcolor="#ffcc99"><font face="Arial"><font size=2><b><a href="https://www.lyngsat.com/tvchannels/ao/Blast.html">Blast</a></b></td>
+    # ....
     
     RESULT=""
+    
     while IFS= read -r LINE; do
-        # if the SID was found at $LINE:
-        if grep -q 'align' <<< $LINE; then
-            SID=$(echo $LINE | cut -d '>' -f 3 | cut -d '<' -f 1)
-            SIDHEX=$(printf "%04X" $SID)                    # converting a decimal value to hexadecimal
-            RESULT="${RESULT}${2}:${SIDHEX}|${3}|"
-        fi
-        # if the CHANNEL name was found at the $LINE:
-        if grep -qv 'align' <<< $LINE; then
-            CHN=$(echo $LINE | grep -oE 'html">.*' | cut -d '>' -f 2 | cut -d '<' -f 1)
-            [ -n "$SID" -a -n "$CHN" ] && RESULT="${RESULT}${CHN}\n"          # only if $CHN variable contains some data --and-- also if $SID contained before any data -> then to add a new item to the $RESULT
-            SID=""
-        fi
-    done <<< "$LIST"                                        # the '.srvid' file format is, for example :   "CAID1,CAID2,CAID3:SID|PROVIDER-NAME|CHANNEL-NAME"
+        SIDHEX=""
+        CHN=""
+        # ServiceID at the odd $LINE:
+        SID=$(echo $LINE | cut -d '>' -f 3 | cut -d '<' -f 1)
+        SIDHEX=$(printf "%04X" $SID)        # converting a decimal value to hexadecimal
+        # CHANNEL-name at the even $LINE:
+        IFS= read -r LINE
+        CHN=$(echo $LINE | grep -oE 'html">.*' | cut -d '>' -f 2 | cut -d '<' -f 1)
+        # write a new entry - if both variables are not empty:
+        [ -n "$SIDHEX" -a -n "$CHN" ] && RESULT="${RESULT}${2}:${SIDHEX}|${3}|${CHN}\n"         # [ -n "$SIDHEX" -a -n "$CHN" ] && RESULT="${RESULT}CAID1,CAID2:${SIDHEX}|PROVIDER|${CHN}\n"
+    done <<< "$LIST"   # the '.srvid' file format is, for example :   "CAID1,CAID2,CAID3:SID|PROVIDER-NAME|CHANNEL-NAME"
     
     # write $RESULT to a temporary file (also taking into account newline codes)
     echo -e "$RESULT" > "/tmp/oscam__${1}.srvid"
