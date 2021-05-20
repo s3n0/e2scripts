c04a83dce7bbebc5578a2780d403d8c3bca549d5
s3n0 <s3n0@users.noreply.github.com>
2021-05-20 19:44:12 +0200

    Update oscam-picons-converter_OscamWebif_script.sh

--- oscam-picons-converter_OscamWebif_script.sh
+++ oscam-picons-converter_OscamWebif_script.sh
@@ -1,13 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-PYSCRIPT="/usr/script/oscam-picons-converter.py"
-#PARAMS="-d -q -1 -c 0624,0D96,FFFE"
-PARAMS="-d -q -a -c 0624,0D96,FFFE"               # 0624,0D96 = Skylink sat-provider, FFFE = Free-To-Air channels (for dvbapi user in OscamWebif)
+# First you need to add to the 'oscam.conf' file, an entry to specify the shell script directory ... for example:
+#     httpscript = /usr/script
+# Then you need to place the script in this folder, including the script for converting to OscamWebif picon format.
+# Both shell-scripts must have execution attributes assigned to them ... for example:
+#     chmod a+x /usr/script/oscam-picons*.sh
+# You can set the parameters for picon conversion according to yourself, i.e. $SCR_PARAMS variable.
+# The script is run in OscamWebif via the 'Script' menu.
 
-/usr/bin/python $PYSCRIPT $PARAMS > /tmp/oscam-picons-converter.log 2>&1 &
+SCR_NAME="oscam-picons-converter.py"
+#SCR_PARAMS="-d -q -1 -c 0624,0D96,FFFE"
+SCR_PARAMS="-d -q -a -c 0624,0D96,FFFE"               # 0624,0D96 = Skylink sat-provider, FFFE = Free-To-Air channels (for dvbapi user in OscamWebif)
+SCR_PATH="/usr/script"
+LOG_FILE="/tmp/${SCR_NAME%.*}.log"
 
-echo "The python script 'oscam-picons-converter.py' has been started - it will run in the background."
-echo "To verify that the script is working properly, you can check the '/tmp/oscam-picons-converter.log' file,"
+[ -f "${SCR_PATH}/${SCR_NAME}" ] || { echo "Error ! The script-file '${SCR_PATH}/${SCR_NAME}' was not found !"; exit 1; }
+
+/usr/bin/python $SCR_PATH/$SCR_NAME $SCR_PARAMS > $LOG_FILE 2>&1 &
+
+echo "The python script '${SCR_NAME}' has been started - it will run in the background."
+echo "To verify that the script is working properly, you can check the '${LOG_FILE}' file,"
 echo "after a few minutes or seconds (depending on the picons generation speed)."
 echo "Bye."
 
